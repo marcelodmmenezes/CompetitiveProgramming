64c4750bd3edcfa5ef3cdef03427e30aa0b3695b
marcelodmmenezes <marcelodmmenezes@gmail.com>
2019-03-04 10:13:58 -0300

    Solved codeforces 281A

---
+++ Sequential/SolvedProblems/CodeForces/A/281.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	string str;
+
+	cin >> str;
+	str[0] = toupper(str[0]);
+
+	cout << str << endl;
+}
