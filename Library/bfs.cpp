20ca3397f6a80dd7dd674ff1d0c0fa20623672eb
marcelodmmenezes <marcelodmmenezes@gmail.com>
2019-05-02 10:11:53 -0300

    bfsDist

--- Library/bfs.cpp
+++ Library/bfs.cpp
@@ -25,6 +25,32 @@ void bfs(vector<vector<int>>& g, int node) {
 	}
 }
 
+void bfsDist(vector<vector<int>>& g, int node) {
+	const int INF = 1000000000;
+
+	queue<int> q;
+	vector<int> dist(g.size(), INF);
+
+	q.push(node);
+	dist[node] = 0;
+
+	while (!q.empty()) {
+		node = q.front();
+		q.pop();
+
+		cout << node << " " << dist[node] << '\n'; // For testing
+
+		for (int i = 0; i < g[node].size(); ++i) {
+			int current = g[node][i];
+
+			if (dist[current] == INF) {
+				q.push(current);
+				dist[current] = dist[node] + 1;
+			}
+		}
+	}
+}
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
@@ -45,4 +71,5 @@ int main() {
 
 	bfs(graph, 0);
 	cout << '\n';
+	bfsDist(graph, 0);
 }
